88987b2432b8af1d2074b82d49d1a3ff0f02cee5
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-11-05 05:03:50 +0500

    Task2

---
+++ Tyuiu.MartysevichAD.Sprint4.Task2.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MartysevichAD.Sprint4.Task2.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MartysevichAD.Sprint4.Task2.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("122539d6-7dd4-454a-b2bd-3e3cd5bc1b13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MartysevichAD.Sprint4.Task2.V30/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MartysevichAD.Sprint4.Task2.V30.Lib;
+
+namespace Tyuiu.MartysevichAD.Sprint4.Task2.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Мартысевич А. Д. | ИИПб-23-3";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #4                                                              *");
+            Console.WriteLine("* Тема:  Одномерные массивы (генератор случайных чисел)                  *");
+            Console.WriteLine("* Задание #2                                                             *");
+            Console.WriteLine("* Вариант #30                                                            *");
+            Console.WriteLine("* Выполнил: Мартысевич Андрей Денисович | ИИПб-23-3                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 8                          *");
+            Console.WriteLine("* подсчитать произведение нечетных элементов массива.                    *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            int len;
+            Console.WriteLine("Введите количество элементов мвссива");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[len];
+            for (int i = 0; i <= len - 1; i++)
+            {
+                array[i] = rnd.Next(2,8);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine(array[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Произведение нечетных элементов =  " + ds.Calculate(array));
+            Console.ReadKey();
+        }
+    }
+}

7726d0a5c6d38ab95d53edca4dc085afbf27ff45
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-11-12 03:02:03 +0500

    Task5

---
+++ Tyuiu.MartysevichAD.Sprint4.Task5.V6.Test/DataServicetest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MartysevichAD.Sprint4.Task5.V6.Lib;
+
+namespace Tyuiu.MartysevichAD.Sprint4.Task5.V6.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[5, 5] { { 9, 6, 9, 3, 7 }, { 3, 3, 3, 8, 2 }, { 2, 1, 3, 5, 2 }, { 6, 2, 3, 2, 5 }, { 4, 5, 6, 9, 5 } };
+            int[,] res = ds.Calculate(matrix);
+            int[,] wait = { { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 } };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
